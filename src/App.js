001510dc1005644b352b8c60bda4015ba62b8adf
dianamariasilva <diana.silva.v94@gmail.com>
2017-10-10 11:04:23 -0500

    estructura profesor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {
+	BrowserRouter,
+	Route,
+	Switch,
+	NavLink,
+	Redirect
+} from 'react-router-dom'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome DEE</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Home from './Home';
+import SignUp from './SignUp';
+import SignUpForm from './SignUpForm';
+import LyftMap from './LyftMap';
+
+const NotFound = (props) => {
+	return (
+		<div><h2> Error 404! </h2></div>
+	);
+}
+
+const App = (props) => {
+	const {model} =  props;
+	console.log('app.props', props)
+	return (<BrowserRouter>
+		<div>
+			<Switch>
+				<Route exact path="/"
+				       render={() => <Redirect to= {'/home'}/>}/>
+				<Route  path="/home" render={() => <Home model={model} />}/>
+				<Route  path="/signup" render={() => <SignUp model={model} />}/>
+				<Route  path="/signup-form" render={() => <SignUpForm model={model} />}/>
+
+				<Route  path="/lyftmap" render={() => <LyftMap model={model} />}/>
+
+				<Route component={NotFound}/>
+			</Switch>
+		</div>
+	</BrowserRouter>)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LyftModel from './LyftModel';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const model = new LyftModel();
+const render = () => {
+	ReactDOM.render(<App model = {model}/>, document.getElementById('root'));
+}
+model.subscribe(render);
+render();
+
 registerServiceWorker();
